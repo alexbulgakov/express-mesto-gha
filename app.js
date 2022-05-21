a5361b865844f81cda6841417946173611a73f67
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-05-21 17:18:55 +0700

    feat: создал контроллеры и роуты для карточек

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
