239d952635334bc7ecfea072b516ef89b29b55c5
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-05-21 21:34:21 +0700

    fix: внёс коррективы в роуты карточек

--- routes/cards.js
+++ routes/cards.js
@@ -13,3 +13,5 @@ router.post('/', createCard);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
+
+module.exports = router;
