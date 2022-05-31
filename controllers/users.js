ffad576523ba5937ec630f58565f395fca1c1998
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-05-31 17:31:25 +0700

    fix: исправил ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -91,7 +91,6 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: true,
       });
       res.send({ token });
     })
