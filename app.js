a2e718dd17ab65b3b27f37ff1fce7a9becb7ed9b
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-05-31 16:44:56 +0700

    fix: исправил ошибки

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./errors/errorHandler');
+const NotFoundError = require('./errors/NotFoundError');
 require('dotenv').config();
 
 const { PORT } = process.env;
@@ -13,24 +15,27 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
+  autoIndex: true,
 });
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 
 app.use(auth);
-app.use(errors);
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.all('*', (req, res) => {
-  res.status(404).send({ message: 'Неправильный путь' });
+app.all('*', (req, res, next) => {
+  next(new NotFoundError('Неверный путь'));
 });
 
+app.use(errors);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
