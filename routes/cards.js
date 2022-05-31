af472c97f846ccf1ae999c47496295388c3daba4
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-05-31 16:30:08 +0700

    fix: исправил ошибку

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required.min(2).max(30),
+      name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().pattern(regExp),
     }),
   }),
