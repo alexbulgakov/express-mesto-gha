00f5d52c977ce81658df1749c715c8597a6a1f4f
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-06-01 05:07:51 +0700

    fix: исправил ошибки авторизации

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./errors/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
+const { createUser, login } = require('./controllers/users');
+const errorWays = require('./routes/errorWays');
+
+const regExp = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
@@ -14,17 +19,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(regExp),
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
+);
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login,
+);
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   autoIndex: true,
 });
 
-app.use('/', require('./routes/auth'));
+// app.use('/', require('./routes/auth'));
 
 app.use(auth);
 
-app.use('/users', require('./routes/users'));
+app.use(errorWays);
+
+// app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.all('*', (req, res, next) => {
